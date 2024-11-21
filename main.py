dfca15db493eaf0ad81cfe30bd86f0b896fa026d
sergiherradorITB <146918553+sergiherradorITB@users.noreply.github.com>
2024-11-21 11:13:36 +0100

    verificacion del nombre

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ class SpriteKind:
     Building = SpriteKind.create()
 
 def on_on_overlap(sprite, otherSprite):
+    global mainName
     cursor.say_text("Press A to play")
     if controller.A.is_pressed():
+        if mainName == "" or mainName == "undefined":
+            mainName = game.ask_for_string("Username", 8)
         storyMode()
 sprites.on_overlap(SpriteKind.player, SpriteKind.storyButton, on_on_overlap)
 
@@ -132,6 +135,7 @@ def TwoPlayersScreen():
 def storyMode():
     global mom2, DialogMode
     storyModeDestroy()
+    createPlayer()
     scene.set_background_image(img("""
         ................................................................................................................................................................
                 ................................................................................................................................................................
@@ -476,7 +480,6 @@ def doMenu():
 def mapLevel():
     global tienda, edificio
     destroyLevelOne()
-    createPlayer()
     tiles.set_current_tilemap(tilemap("""
         level4
     """))
@@ -692,9 +695,6 @@ def mapLevel():
                     ddddddddddddddddddddddddddeeeeedddddddddddddddddddddddddd.
         """),
         SpriteKind.Building)
-    tiles.place_on_random_tile(tienda, assets.tile("""
-        myTile13
-    """))
     tiles.place_on_tile(player_1, tiles.get_tile_location(1, 9))
     scene.camera_follow_sprite(player_1)
 def createPlayer():
@@ -731,35 +731,22 @@ sprites.on_overlap(SpriteKind.player,
     on_on_overlap2)
 
 def on_on_overlap3(sprite2, otherSprite2):
-    global DialogMode, mom2
+    global DialogMode
     DialogMode = True
     game.show_long_text("Talk with mom", DialogLayout.BOTTOM)
+    story.print_character_text("" + mainName + "!" + " They took it... They took everything from me!",
+        "Mom")
+    story.print_character_text("\"Who, Ma? What happened?\"", mainName)
+    story.print_character_text("\"That gang... Those thieves! They stormed in, took my jewelry, my savings... everything! You gotta do something!\"",
+        "Mom")
+    story.print_character_text("\"Don't worry, Ma. I'll find them. They won't get away with this.\"",
+        mainName)
     story.show_player_choices("Get Out", "Stay")
     if story.check_last_answer("Get Out"):
         mom2.set_kind(SpriteKind.Complete)
-        game.show_long_text("Yendo a la calle...", DialogLayout.BOTTOM)
         mapLevel()
     elif story.check_last_answer("Stay"):
-        mom2.set_kind(SpriteKind.Complete)
-        mom2 = sprites.create(img("""
-                . . . . . . . f f . . . . . . . 
-                            . . . . . f f 4 4 f f . . . . . 
-                            . . . . f 5 4 5 5 4 5 f . . . . 
-                            . . . f e 4 5 5 5 5 4 e f . . . 
-                            . . f b 3 e 4 4 4 4 e 3 b f . . 
-                            . f e 3 3 3 3 3 3 3 3 3 3 e f . 
-                            . f 3 3 e b 3 e e 3 b e 3 3 f . 
-                            . f b 3 f f e e e e f f 3 b f . 
-                            f f b b f b f e e f b f b b f f 
-                            f b b b e 1 f 4 4 f 1 e b b b f 
-                            . f b b f 4 4 4 4 4 e e b b f . 
-                            . . f e f b d d d e 4 4 4 f . . 
-                            . . e 4 c d d d d e 4 4 e f . . 
-                            . . e f b b d b d d e e f . . . 
-                            . . . f f 1 1 d 1 d 1 f f . . . 
-                            . . . . . f b b f f f . . . . .
-            """),
-            SpriteKind.Complete)
+        pause(1000)
 sprites.on_overlap(SpriteKind.player, SpriteKind.mom, on_on_overlap3)
 
 def destroyLevelOne():
@@ -771,8 +758,9 @@ mom2: Sprite = None
 player_1: Sprite = None
 two_players_button: Sprite = None
 single_player_button: Sprite = None
+mainName = ""
 cursor: Sprite = None
-mapLevel()
+doMenu()
 
 def on_on_update():
     if story.is_menu_open():
