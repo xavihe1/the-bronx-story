24d419f1c166086af6b23154a1846eaa13b92d88
xavihe1 <146827255+xavihe1@users.noreply.github.com>
2024-11-21 17:14:40 +0100

    nuevo estilo de la madre

--- main.ts
+++ main.ts
@@ -266,21 +266,21 @@ function storyMode () {
     tiles.placeOnTile(player_1, tiles.getTileLocation(7, 12))
     mom2 = sprites.create(img`
         . . . . . . . f f . . . . . . . 
-        . . . . . f f 4 4 f f . . . . . 
-        . . . . f 5 4 5 5 4 5 f . . . . 
-        . . . f e 4 5 5 5 5 4 e f . . . 
-        . . f b 3 e 4 4 4 4 e 3 b f . . 
-        . f e 3 3 3 3 3 3 3 3 3 3 e f . 
-        . f 3 3 e b 3 e e 3 b e 3 3 f . 
-        . f b 3 f f e e e e f f 3 b f . 
-        f f b b f b f e e f b f b b f f 
-        f b b b e 1 f 4 4 f 1 e b b b f 
-        . f b b f 4 4 4 4 4 e e b b f . 
-        . . f e f b d d d e 4 4 4 f . . 
-        . . e 4 c d d d d e 4 4 e f . . 
-        . . e f b b d b d d e e f . . . 
-        . . . f f 1 1 d 1 d 1 f f . . . 
-        . . . . . f b b f f f . . . . . 
+        . . . . . f f 3 3 f f . . . . . 
+        . . . . f 3 3 3 3 3 3 f . . . . 
+        . . . f 3 3 3 3 3 3 3 3 f . . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . f 3 3 e 3 3 e e 3 3 e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        f f 3 3 f b f e e f b f 3 3 f f 
+        f 3 3 3 e 1 f e e f 1 e 3 3 3 f 
+        . f 3 3 f e e e e e e e 3 3 f . 
+        . . f e f d d d d d d d e f . . 
+        . . e e c d d d d d d d e e . . 
+        . . e f b d d d d d d d f e . . 
+        . . . f f 1 d d 1 d 1 f f . . . 
+        . . . . . f f f f f f . . . . . 
         `, SpriteKind.mom)
     tiles.placeOnTile(mom2, tiles.getTileLocation(15, 2))
     isTalking = false
